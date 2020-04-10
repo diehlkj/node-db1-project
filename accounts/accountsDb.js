3c9c8547b2a395c6f2c313e3e9d1063490880258
Ken Diehl <diehlkj@BookBook.local>
2020-04-10 16:59:26 -0400

    Created accountsDb and accountsRouter. Completed accountsDb, working on router now

---
+++ accounts/accountsDb.js
@@ -0,0 +1,36 @@
+module.exports = {
+    get,
+    getById,
+    insert,
+    update,
+    remove
+}
+
+function get() {
+    return db('accounts');
+}
+
+function getById(id) {
+    return db('accounts')
+    .where({ id });
+}
+
+function insert(account) {
+    return db('accounts')
+    .insert(account)
+    .then(newIds => {
+        return getById(newIds[0]);
+    });
+}
+
+function update(id, changes) {
+    return db('accounts')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+    return db('accounts')
+    .where('id', id )
+    .del();
+}
